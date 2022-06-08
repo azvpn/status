e26d621da34fa27e6d8a7f6ad0c047e148f79e36
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-06-08 09:39:39 +0700

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo "                          █████  ███████ ██    ██ ██████  ███    ██    ███    ███ ███████" 
+echo "                         ██   ██    ███  ██    ██ ██   ██ ████   ██    ████  ████ ██"      
+echo "                         ███████   ███   ██    ██ ██████  ██ ██  ██    ██ ████ ██ ████"█   
+echo "                         ██   ██  ███     ██  ██  ██      ██  ██ ██    ██  ██  ██ ██"      
+echo "                         ██   ██ ███████   ████   ██      ██   ████ ██ ██      ██ ███████" 
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "                                                                Vui Lòng Nhập Key Của Bạn !"
-echo "                                                      Nếu Không Có Key Vui Lòng Liên Hệ ADMIN SPEED4G.XYZ"
-echo "                                                               ZALO: 0333725953 (Nguyễn Nghị)"
+echo "                                            Vui Lòng Nhập Key Của Bạn !"
+echo "                                 Nếu Không Có Key Vui Lòng Liên Hệ ADMIN AZVPN.ME"
+echo "                                        ZALO: 0982708204 (Nguyễn Mạnh Long)"
 echo -e ""
 echo -e ""
 read -p "Key: " key
 
-if [[ $key == nguyenvannghi ]]; then
+if [[ $key == long ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonspeed4g.sh)
-elif [[ $key == dangvanhoang ]]; then
+bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/luachonazvpn.sh)
+elif [[ $key == azvpn ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonvpndata.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/luachonazvpn.sh)
 else 
 sleep 2
 echo "Key Không Chính Xác !"
