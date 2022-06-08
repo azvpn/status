16adfd213bea2c77c2b6ced88c15cbe6a5e53c22
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-06-08 09:42:47 +0700

    Update luachonspeed4g.sh

--- luachonspeed4g.sh
+++ luachonspeed4g.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo "                             █████  ███████ ██    ██ ██████  ███    ██    ███    ███ ███████" 
+echo "                            ██   ██    ███  ██    ██ ██   ██ ████   ██    ████  ████ ██"      
+echo "                            ███████   ███   ██    ██ ██████  ██ ██  ██    ██ ████ ██ ████"█   
+echo "                            ██   ██  ███     ██  ██  ██      ██  ██ ██    ██  ██  ██ ██"      
+echo "                            ██   ██ ███████   ████   ██      ██   ████ ██ ██      ██ ███████" 
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "                                                                Vui Lòng Lựa Chọn Phương Thức !"
+echo "                                                  Vui Lòng Lựa Chọn Phương Thức !"
 echo -e ""
-echo  "         ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
-echo  "         ██                                                                  ██                                                                          ██"
-echo  "         ██                          1. V2RAY                                ██                                2. TROJAN                                 ██"
-echo  "         ██                                                                  ██                                                                          ██"
-echo  "         ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
+echo  "      ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
+echo  "      ██                                                      ██                                                            ██"
+echo  "      ██                  1. V2RAY                            ██                        2. TROJAN                           ██"
+echo  "      ██                                                      ██                                                            ██"
+echo  "      ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
 echo -e ""
 echo -e ""
 echo -e ""                                                                                                      
     echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/v2ray/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
+        1) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/azvpn/config/main/azvpn/v2ray/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
-        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/speed4g/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
+        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/azvpn/config/main/azvpn/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/Nghi235/status/blob/main/luachonspeed4g.sh)
         ;;
